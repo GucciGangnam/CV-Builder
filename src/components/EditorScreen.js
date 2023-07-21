a756fc76c91967bdc97c0bd6654b2f1a9c4f0292
Gucci Gangnam <diviner02hive@icloud.com>
2023-07-21 18:35:21 +0700

    UseStates all set up and uswer inputs are show in preview screen - NEXT: add char limits and style restrictions to stop user manipulating pagesetup. - THEN: add buttons to add or remove new fields. -THEN: Style and add theme options

--- src/components/EditorScreen.js
+++ src/components/EditorScreen.js
@@ -11,6 +11,30 @@ import { useState } from 'react';
 
 export const EditorScreen = () => {
 
+const [name, setName] = useState('Your Full Name');
+const [profession, setProfession] = useState('Profession');
+const [profile, setProfile] = useState('Write about yourself');
+
+const [number, setNumber] = useState('Phone Number');
+const [email, setEmail] = useState('Email');
+const [address, setAddress] = useState('Address');
+
+const [skill1, setSkill1] = useState('Skill 1');
+const [skill2, setSkill2] = useState('Skill 2');
+const [skill3, setSkill3] = useState('Skill 3');
+const [skill4, setSkill4] = useState('Skill 4');
+
+const [university, setUniversity] = useState('University Name');
+const [degree, setDegree] = useState('Degree');
+const [year, setYear] = useState('Year');
+
+const [role1, setRole1] = useState('Most Recent Role');
+const [company1, setCompany1] = useState('Company');
+const [description1, setDescription1] = useState('A brief description about your role');
+
+const [role2, setRole2] = useState('Most Recent Role');
+const [company2, setCompany2] = useState('Company');
+const [description2, setDescription2] = useState('A brief description about your role');
 
 
     return ( 
@@ -28,19 +52,22 @@ export const EditorScreen = () => {
                 <div className="TopColumn">
                     <input className='NameInput'
                             type="text" 
-                            placeholder='Full Name'
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
                             required
                         />
                     <input className='ProfessionInput' 
                             type="text"
-                            placeholder='Your profession'
+                            value={profession}
+                            onChange={(e) => setProfession(e.target.value)}
                             required
                         />
                     <h2>Profile</h2>
                     <textarea className='ProfileInput'
                             type="text"
-                            placeholder='hello'
+                            value={profile}
                             rows="5"
+                            onChange={(e) => setProfile(e.target.value)}
                             required
                         />
                 </div>
@@ -49,39 +76,46 @@ export const EditorScreen = () => {
                     <h3>Contact</h3>
                     <input className='ContactInput'
                             type="text"
-                            placeholder='Phone Number'
+                            value={number}
+                            onChange={(e) => setNumber(e.target.value)}
                             required
                         />
                     <input className='ContactInput'
                             type="text"
-                            placeholder='Email'
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
                             required
                         />
                     <input className='ContactInput'
                             type="text"
-                            placeholder='Address'
+                            value={address}
+                            onChange={(e) => setAddress(e.target.value)}
                             required
                         />
 
                     <h3>Skills</h3>
                     <input className='SkillsInput'
                             type="text"
-                            placeholder='Skill 1'
+                            value={skill1}
+                            onChange={(e) => setSkill1(e.target.value)}
                             required
                         />
                     <input className='SkillsInput'
                             type="text"
-                            placeholder='Skill 2'
+                            value={skill2}
+                            onChange={(e) => setSkill2(e.target.value)}
                             required
                         />
                     <input className='SkillsInput'
                             type="text"
-                            placeholder='Skill 3'
+                            value={skill3}
+                            onChange={(e) => setSkill3(e.target.value)}
                             required
                         />
                     <input className='SkillsInput'
                             type="text"
-                            placeholder='Skill 4'
+                            value={skill4}
+                            onChange={(e) => setSkill4(e.target.value)}
                             required
                         />
                         
@@ -89,17 +123,20 @@ export const EditorScreen = () => {
                     <h3>Education</h3>
                     <input className='EducationInput'
                             type="text"
-                            placeholder='University Name'
+                            value={university}
+                            onChange={(e) => setUniversity(e.target.value)}
                             required
                         />
                     <input className='EducationInput'
                             type="text"
-                            placeholder='Degree'
+                            value={degree}
+                            onChange={(e) => setDegree(e.target.value)}
                             required
                         />
                     <input className='EducationInput'
                             type="text"
-                            placeholder='Year'
+                            value={year}
+                            onChange={(e) => setYear(e.target.value)}
                             required
                         />
                 </div>
@@ -108,47 +145,38 @@ export const EditorScreen = () => {
                 <h3>Experience</h3>
                 <input className='ExperienceInput'
                             type="text"
-                            placeholder='Most Recdent role'
+                            value={role1}
+                            onChange={(e) => setRole1(e.target.value)}
                             required
                         />
                     <input className='ExperienceInput'
                             type="text"
-                            placeholder='Company'
+                            value={company1}
+                            onChange={(e) => setCompany1(e.target.value)}
                             required
                         />
                     <input className='ExperienceInput'
                             type="text"
-                            placeholder='A brief description about your role'
+                            value={description1}
+                            onChange={(e) => setDescription1(e.target.value)}
                             required
                         />
                 <input className='ExperienceInput'
                             type="text"
-                            placeholder='Most Recdent role'
+                            value={role2}
+                            onChange={(e) => setRole2(e.target.value)}
                             required
                         />
                     <input className='ExperienceInput'
                             type="text"
-                            placeholder='Company'
+                            value={company2}
+                            onChange={(e) => setCompany2(e.target.value)}
                             required
                         />
                     <input className='ExperienceInput'
                             type="text"
-                            placeholder='A brief description about your role'
-                            required
-                        />
-                <input className='ExperienceInput'
-                            type="text"
-                            placeholder='Most Recdent role'
-                            required
-                        />
-                    <input className='ExperienceInput'
-                            type="text"
-                            placeholder='Company'
-                            required
-                        />
-                    <input className='ExperienceInput'
-                            type="text"
-                            placeholder='A brief description about your role'
+                            value={description2}
+                            onChange={(e) => setDescription2(e.target.value)}
                             required
                         />
                 </div>
@@ -164,40 +192,40 @@ export const EditorScreen = () => {
 
             
                 <div className="TopColumn">
-                    <h1>FirstName</h1>
-                    <p>Your Profession </p>
+                    <h1>{name}</h1>
+                    <p>{profession} </p>
                     <h2>Profile</h2>
-                    <p>hello</p>
+                    <p>{profile}</p>
                 </div>
 
                 <div className='LeftColumn'>
                     <h3>Contact</h3>
-                    <p>Phone Number</p>
-                    <p>E-mail address</p>
-                    <p>Address</p>
+                    <p>{number}</p>
+                    <p>{email}</p>
+                    <p>{address}</p>
 
                     <h3>Skills</h3>
-                    <p>Skill 1</p>
-                    <p>Skill 2</p>
-                    <p>Skill 3</p>
-                    <p>Skill 4</p>
+                    <p>{skill1}</p>
+                    <p>{skill2}</p>
+                    <p>{skill3}</p>
+                    <p>{skill4}</p>
                     
                         
 
                     <h3>Education</h3>
-                    <p>University Name</p>
-                    <p>Degree</p>
-                    <p>Year</p>
+                    <p>{university}</p>
+                    <p>{degree}</p>
+                    <p>{year}</p>
                 </div>
 
                 <div className='BodyColumn'>
                 <h3>Experience</h3>
-                <p>Most Recent role</p>
-                <p>Company</p>
-                <p>A brief description about your role</p>
-                <p>Most Recent role</p>
-                <p>Company</p>
-                <p>A brief description about your role</p>
+                <p>{role1}</p>
+                <p>{company1}</p>
+                <p>{description1}</p>
+                <p>{role2}</p>
+                <p>{company2}</p>
+                <p>{description2}</p>
                 
                 
                 </div>
